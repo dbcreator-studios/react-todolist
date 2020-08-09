b9a2a9577f85da80bb46c6a202abc1b35fb5abe0
Wildhan Ellan <61819990+dbcreator-studios@users.noreply.github.com>
2020-08-09 14:27:17 +0700

    Menambahkan Header TodoForm Todos

---
+++ src/components/Todos.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Todos = () => {
+  return (
+    <section className="todos">
+      <div className="todo">
+        <div className="todo-text">React Introduction</div>
+      </div>
+      <div className="todo">
+        <div className="todo-text">React Introduction</div>
+      </div>
+      <div className="todo">
+        <div className="todo-text">React Introduction</div>
+      </div>
+      <div className="todo">
+        <div className="todo-text">React Introduction</div>
+      </div>
+      <div className="todo">
+        <div className="todo-text">React Introduction</div>
+      </div>
+      <div className="todo">
+        <div className="todo-text">React Introduction</div>
+      </div>
+      <div className="todo">
+        <div className="todo-text">React Introduction</div>
+      </div>
+    </section>
+  );
+};
+
+export default Todos;
