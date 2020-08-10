2ba2521161d8043f7354ac2c21bbae53f085a964
Wildhan Ellan <61819990+dbcreator-studios@users.noreply.github.com>
2020-08-10 19:55:28 +0700

    Membuat pages dan component todo

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,29 +1,45 @@
 import React from "react";
+import Todo from "./Todo";
 
 const Todos = () => {
+  const todos = [
+    {
+      text: "1. Semangat Belajar"
+    },
+    {
+      text: "2. Semangat Belajar"
+    },
+    {
+      text: "3. Semangat Belajar"
+    },
+    {
+      text: "4. Semangat Belajar"
+    },
+    {
+      text: "5. Semangat Belajar"
+    },
+    {
+      text: "6. Semangat Belajar"
+    },
+    {
+      text: "7. Semangat Belajar"
+    },
+    {
+      text: "8. Semangat Belajar"
+    },
+    {
+      text: "9. Semangat Belajar"
+    },
+    {
+      text: "10. Semangat Belajar"
+    }
+  ];
+
   return (
     <section className="todos">
-      <div className="todo">
-        <div className="todo-text">React Introduction</div>
-      </div>
-      <div className="todo">
-        <div className="todo-text">React Introduction</div>
-      </div>
-      <div className="todo">
-        <div className="todo-text">React Introduction</div>
-      </div>
-      <div className="todo">
-        <div className="todo-text">React Introduction</div>
-      </div>
-      <div className="todo">
-        <div className="todo-text">React Introduction</div>
-      </div>
-      <div className="todo">
-        <div className="todo-text">React Introduction</div>
-      </div>
-      <div className="todo">
-        <div className="todo-text">React Introduction</div>
-      </div>
+      {todos.map((todo) => {
+        return <Todo text={todo.text} />;
+      })}
     </section>
   );
 };
